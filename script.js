d4112aea97ad7e19d52dad7922e68c9d4a0604e6
Aditya Butola <butola.aditya10@gmail.com>
2023-05-12 22:59:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const prices = document.querySelectorAll('[data-ns-test="prices"]');
+const grandTotal = document.querySelector('[data-ns-test="grandTotal"]');
+
+let total = 0;
+
+prices.forEach(price => {
+  total += parseFloat(price.textContent);
+});
+
+grandTotal.textContent = total.toFixed(2);
